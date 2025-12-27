c6b06460a4f1a77bff46be5604bd272a0bb64079
Cemal Akdeniz <cemal@Mac.communityfibre.co.uk>
2025-12-27 22:09:08 +0000

    changed to jsoup

--- src/main/java/com/tiktok/TikTokMain.java
+++ src/main/java/com/tiktok/TikTokMain.java
@@ -1,6 +1,11 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+
 
 
 void main() {
@@ -16,10 +21,14 @@ void main() {
     System.out.println(client.getClass().getName());
     try {
         HttpResponse<String> cevap = client.send(rr, HttpResponse.BodyHandlers.ofString());
-        int start=cevap.body().indexOf("downloadAddr");
-        String downloadadrString = cevap.body().substring(start);
+        Document document=Jsoup.parse(cevap.body());
+        Element jsonElement=document.getElementById("__UNIVERSAL_DATA_FOR_REHYDRATION__");
+       String jsonData= jsonElement.data();
+
+        int start=jsonData.indexOf("downloadAddr");
+        String downloadadrString = jsonData.substring(start);
         int end = downloadadrString.indexOf("t_chain_token");
-        String downloadUrl = cevap.body().substring(start+15, start+end+13);
+        String downloadUrl = jsonData.substring(start+15, start+end+13);
         String replaced=downloadUrl.replace("\\u002F","/");
         HttpRequest videoRequest= HttpRequest
                 .newBuilder()
