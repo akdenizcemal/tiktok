78b307022f1d4aa74a247fd479f6860f7176301c
Cemal Akdeniz <cemal@Mac.communityfibre.co.uk>
2025-12-27 23:09:03 +0000

    used Jackson library

--- src/main/java/com/tiktok/TikTokMain.java
+++ src/main/java/com/tiktok/TikTokMain.java
@@ -1,14 +1,17 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+
 
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 
-
-void main() {
+void main() throws IOException, InterruptedException {
     String pageUrl = "https://www.tiktok.com/@livealoha1230/video/7587966822691966222";
 
 
@@ -19,33 +22,35 @@ void main() {
             .build();
     var client = HttpClient.newHttpClient();
     System.out.println(client.getClass().getName());
-    try {
-        var jsonData = extractUniversalData(client, rr);
+    var jsonData = extractUniversalData(client, rr);
+    var downloadAddr = extractDownloadAddr(jsonData);
 
-        int start=jsonData.indexOf("downloadAddr");
-        var downloadadrString = jsonData.substring(start);
-        int end = downloadadrString.indexOf("t_chain_token");
-        var downloadUrl = jsonData.substring(start+15, start+end+13);
-        var replaced=downloadUrl.replace("\\u002F","/");
-        var videoRequest= HttpRequest
-                .newBuilder()
-                .GET()
-                .uri(URI.create(replaced))
-                .build();
-        HttpResponse<byte[]> respp=client.send(videoRequest,HttpResponse.BodyHandlers.ofByteArray());
-        System.out.println(respp.statusCode());
-    }catch (Exception ex){
-        System.out.println(ex);
-    }
+    fetchAndStoreVideo(downloadAddr, client);
 
 
 }
 
-private static String extractUniversalData(HttpClient client, HttpRequest rr) throws IOException, InterruptedException {
+private String extractUniversalData(HttpClient client, HttpRequest rr) throws IOException, InterruptedException {
     HttpResponse<String> cevap = client.send(rr, HttpResponse.BodyHandlers.ofString());
-    Document document=Jsoup.parse(cevap.body());
-    Element jsonElement=document.getElementById("__UNIVERSAL_DATA_FOR_REHYDRATION__");
-    String jsonData= jsonElement.data();
-    return jsonData;
+    Document document = Jsoup.parse(cevap.body());
+    Element jsonElement = document.getElementById("__UNIVERSAL_DATA_FOR_REHYDRATION__");
+    return jsonElement.data();
+}
+
+private String extractDownloadAddr(String jsonData) throws JsonProcessingException {
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode root = mapper.readTree(jsonData);
+    return root.get("__DEFAULT_SCOPE__").get("webapp.video-detail").get("itemInfo")
+            .get("itemStruct").get("video").get("downloadAddr").asText();
+}
+
+private void fetchAndStoreVideo(String replaced, HttpClient client) throws IOException, InterruptedException {
+    var videoRequest = HttpRequest
+            .newBuilder()
+            .GET()
+            .uri(URI.create(replaced))
+            .build();
+    HttpResponse<byte[]> respp = client.send(videoRequest, HttpResponse.BodyHandlers.ofByteArray());
+    System.out.println(respp.statusCode());
 }
 
