8351fbea69313c513ada702f7cbbf0ba11bfe4f0
Cemal Akdeniz <cemal@Mac.communityfibre.co.uk>
2025-12-28 17:53:51 +0000

    page url as a parameter

---
+++ src/main/java/com/tiktok/TikTokVideDownloader.java
@@ -0,0 +1,62 @@
+package com.tiktok;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Scanner;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class TikTokVideDownloader {
+
+   public void download(String pageUrl)  {
+        HttpRequest rr = HttpRequest
+                .newBuilder()
+                .GET()
+                .uri(URI.create(pageUrl))
+                .build();
+        var client = HttpClient.newHttpClient();
+        System.out.println(client.getClass().getName());
+       String jsonData = null;
+       try {
+           jsonData = extractUniversalData(client, rr);
+           var downloadAddr = extractDownloadAddr(jsonData);
+           fetchAndStoreVideo(downloadAddr, client);
+       } catch (IOException | InterruptedException e) {
+           throw new RuntimeException(e);
+       }
+    }
+
+    private String extractUniversalData(HttpClient client, HttpRequest rr) throws IOException, InterruptedException {
+        HttpResponse<String> cevap = client.send(rr, HttpResponse.BodyHandlers.ofString());
+        Document document = Jsoup.parse(cevap.body());
+        Element jsonElement = document.getElementById("__UNIVERSAL_DATA_FOR_REHYDRATION__");
+        return jsonElement.data();
+    }
+
+    private String extractDownloadAddr(String jsonData) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(jsonData);
+        return root.get("__DEFAULT_SCOPE__").get("webapp.video-detail").get("itemInfo")
+                .get("itemStruct").get("video").get("downloadAddr").asText();
+    }
+
+    private void fetchAndStoreVideo(String replaced, HttpClient client) throws IOException, InterruptedException, IOException {
+        var videoRequest = HttpRequest
+                .newBuilder()
+                .GET()
+                .uri(URI.create(replaced))
+                .build();
+        HttpResponse<byte[]> respp = client.send(videoRequest, HttpResponse.BodyHandlers.ofByteArray());
+        System.out.println(respp.statusCode());
+    }
+}
+
